e6214a933990307720f71bb6c46bcb57d6af655a
RonaldZhao <zzgdata@gmail.com>
2018-05-09 01:24:58 +0800

    初次备份

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'RonaldZhao的博客',
+    description: '分享知识，记录成长 | Powered By VuePress',
+    head: [
+        ['link', {rel: 'icon', href: `/logo.png`}]
+    ],
+    themeConfig: {
+        nav: [
+            { text: '博客', link: '/blog/' },
+            { text: '关于', link: '/about/' },
+            { text: 'GitHub', link: 'https://github.com/RonaldZhao' },
+        ]
+  }
+}
